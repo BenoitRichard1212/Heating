64e57af0510d927b141bf7c878b8b43bea9ac5f5
BenoitRichard1212 <33323670+BenoitRichard1212@users.noreply.github.com>
2018-09-02 18:50:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,44 @@
 import mysql.connector
 from mysql.connector import Error
 
-db_cursor = None
-db_conn = None
-Profile_name = None
- 
+_db_cursor = None
+_db_conn = None
+
 def connect():
     """ Connect to MySQL database """
     try:
-        db_conn = mysql.connector.connect(host='localhost',
+        _db_conn = mysql.connector.connect(host='localhost',
                                        database='Heating',
                                        user='root',
-                                       password='B3nmal!gn312')
-        if db_conn.is_connected():
+                                       password='')
+        if _db_conn.is_connected():
             print('Connected to MySQL database')
-             db_cursor = db_conn.cursor()
- 
-    except Error as e:
-        print(e)
+            _db_cursor = _db_conn.cursor()
+        except Error as e:
+            print(e)
 
 def close():
-    finally:
-        db_conn.close()
+    _db_conn.close()
 
 def loadGloabalSettings():
+    _db_cursor.execute("SELECT * FROM GlobalConfig")
 
-    db_cursor.execute("SELECT * FROM GlobalConfig")
-
-    result = db_cursor.fetchall()
+    result = _db_cursor.fetchall()
 
     for x in result:
         print(x)
 
 def saveGlobalSettings():
-
-    sql = "INSERT INTO GlobalConfig (Profile_name, Error_threshold, Season_Mode, Precision_degree) VALUES (%s, %d, %s, %d)"
+    sql = "INSERT INTO GlobalConfig (Profile_name, Error_threshold, Season_Mode, Precision_degree) VALUES     (%s, %d, %s, %d)"
     val = ("Maison", 1, "Hiver", 1)
-    db_cursor.execute(sql, val)
+    _db_cursor.execute(sql, val)
 
-    db_conn.commit()
+    _db_conn.commit()
 
-    print(db_cursor.rowcount, "record inserted.")
- 
- 
+    print(_db_cursor.rowcount, "record inserted.")
+
 if __name__ == '__main__':
     connect()
     saveGlobalSettings()
     loadGloabalSettings()
     close()
-x
