f52c736880dfa3921c9e1e3bc579a10d978a14c6
Benoit Richard <benoit.richard@novadba.com>
2018-10-08 13:05:47 -0400

    space changes

--- main.py
+++ main.py
@@ -27,111 +27,71 @@ def connect():
 
 def getPumpRelayStatus():
     connect()
-
     query = "SELECT status FROM relays WHERE name = '%s'" % (pumpRelay.name)
-    
     _db_cursor.execute(query)
-
     result = mycursor.fetchall()
-
     _db_conn.close()
-
     return result
 
 
 def getRelay(name):
     connect()
-    
     query = "SELECT * FROM relays WHERE name = '%s'" % (name)
-    
     _db_cursor.execute(query)
-    
     row = mycursor.fetchall()
-    
     relay = New Relay(row[0], row[1], row[2])
-    
     _db_conn.close()
-
     return relay
 
 
 def getAllRelays():
     relays = []
-    
     connect()
-    
     query = "SELECT * FROM relays"
-    
     cursor.execute(sql)
-        
     rcount = int(cursor.rowcount)
-
     for r in rcount:
         row = cursor.fetchone()
-
         relay = New Relay(row[0], row[1], row[2])
-
         relays.append(relay)
-    
     return relays
 
 
 def getAllRooms():
     rooms = []
-    
     connect()
-    
     query = "SELECT * FROM rooms"
-    
     cursor.execute(sql)
-    
     rcount = int(cursor.rowcount)
-    
     for r in rcount:
         row = cursor.fetchone()
-        
         room = New Room(row[0], row[1], row[2], row[3], row[4])
-        
         rooms.append(room)
-    
     return rooms
 
 
 def getSensorTemp(name):
     connect()
-    
     query = "SELECT temperature FROM temperauredata WHERE sensorName = '%s'" % (name)
-    
     _db_cursor.execute(query)
-    
     result = mycursor.fetchall()
-    
     _db_conn.close()
-    
     return result
 
 
 def openPumpRelay():
     connect()
-    
     query = "UPDATE relays SET status = 'open' WHERE name = '%s';" % (pumpRelay.name)
-    
     _db_cursor.execute(query)
-    
     _db_conn.close()
-    
     GPIO.output(pumpRelay.gpio, GPIO.LOW)
 
 
 def closePumpRelay():
     connect()
-    
     query = "UPDATE relays SET status = 'close' WHERE name = '%s';" % (pumpRelay.name)
-    
-    _db_cursor.execute(query)
-    
+    _db_cursor.execute(query)    
     _db_conn.close()
-    
     GPIO.output(pumpRelay.gpio, GPIO.HIGH)
 
 def openRelay(relay):
@@ -178,7 +138,6 @@ def closeRelay(p_relay):
 if __name__ == '__main__':
     pumpRelay = getPumpRelayStatus()
     rooms = getAllRooms()
-
     for room in rooms
         if (room.relay == "close")
             if (getSensorTemp(room.sensor_wall) < room.temp_min)
